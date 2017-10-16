e172dc01164acdeac33d185b1fc0f0e68d62b760
Ahmed-Abdel-Mgeed <31767300+Ahmed-Abdel-Mgeed@users.noreply.github.com>
2017-10-16 06:07:04 +0200

    Animats

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
-$(function() {
-	'use strict';
+$(document).ready( function() {
+
+	$(function() {
+		'use strict';
 	// Links Add Active Class
 
-	$('.links li').click(function() {
-		$(this).addClass('active').siblings().removeClass('active');
+		$('.links li').click(function() {
+			$(this).addClass('active').siblings().removeClass('active');
+		});
+
+		// Addjuste Header Height
+		var myHeader = $('.head-image');
+
+			myHeader.height($(window).height());
+
+		$(window).resize(function(){
+			myHeader.height($(window).height());
+		});
+
+		/* Slide Down For Divs
+
+		$("#home").on({
+			mouseenter:function() {
+			$(".movie").fadeIn(1500)
+			}
+		});
+
+
+		$("#about").on({
+			mouseenter:function() {
+			
+			}
+		});
+
+		$("#shopping").on({
+			mouseenter:function() {
+			
+			}
+		});
+		*/
+
+		//  Animate My Page
+
+		$("body").animate({
+			top:'0'
+		},4000);
+
+		$(".main .to-use").animate({
+			width:'91%'
+		},6500,function() {
+				$('iframe').fadeIn(2600)
+				$('ul').fadeIn()
+				$("p").fadeIn();
+			});
+		
+
 	});
-
-	// Addjuste Header Height
-	var myHeader = $('.head-image');
-
-	myHeader.height($(window).height());
-
-	$(window).resize(function(){
-		myHeader.height($(window).height());
-	});
-
 
 });
-
 //Google maps api
 
 	function initMap() {
